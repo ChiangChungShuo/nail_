835abbc66a87cf81711dd63a39526748429bb3ec
Chiang ChungShuo <azraelk19@gmail.com>
2023-03-29 17:25:31 +0800

    0329

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/ChiangChungShuo/weekk66.git main:gh-pages
+git push -f https://github.com/ChiangChungShuo/nail_.git main:gh-pages
 
 cd -
